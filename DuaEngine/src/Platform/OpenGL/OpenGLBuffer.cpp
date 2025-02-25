126fea64fef71e051e256a5f7f20f3c5d13b14b2
lxdua <2753792469@QQ.COM>
2025-02-25 15:32:09 +0800

    修改gitignore

--- DuaEngine/src/Platform/OpenGL/OpenGLBuffer.cpp
+++ DuaEngine/src/Platform/OpenGL/OpenGLBuffer.cpp
@@ -9,7 +9,7 @@ namespace Dua {
 
 	OpenGLVertexBuffer::OpenGLVertexBuffer(float* vertices, uint32_t size)
 	{
-		glCreateBuffers(1, &m_RendererID);
+		glGenBuffers(1, &m_RendererID);
 		Bind();
 		glBufferData(GL_ARRAY_BUFFER, size, vertices, GL_STATIC_DRAW);
 	}
@@ -34,7 +34,7 @@ namespace Dua {
 	OpenGLIndexBuffer::OpenGLIndexBuffer(uint32_t* indices, uint32_t count)
 		: m_Count(count)
 	{
-		glCreateBuffers(1, &m_RendererID);
+		glGenBuffers(1, &m_RendererID);
 		Bind();
 		glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(uint32_t), indices, GL_STATIC_DRAW);
 	}
